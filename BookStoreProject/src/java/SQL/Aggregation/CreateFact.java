9ad9139f85c419f5e09c95fcbb1498bcd24f5a9e
qiuyu6 <yuq@YuqdeMacBook-Air.local>
2017-04-16 16:30:34 -0400

    qifei

--- BookStoreProject/src/java/SQL/Aggregation/CreateFact.java
+++ BookStoreProject/src/java/SQL/Aggregation/CreateFact.java
@@ -23,9 +23,9 @@ public class CreateFact {
             conn = SQLConnectSQL.getConn();
             ps = conn.prepareStatement("DROP TABLE IF EXISTS fact");
             ps.executeUpdate();
-            ps = conn.prepareStatement("CREATE TABLE fact(product_id varchar(45) NOT NULL, amount int(11) NOT NULL, store_id varchar(45) NOT NULL, customer_id varchar(45) NOT NULL, time varchar(45), sale float(11) NOT NULL)");            
+            ps = conn.prepareStatement("CREATE TABLE fact (product_id varchar(45) NOT NULL, amount int(11) NOT NULL, store_id varchar(45) NOT NULL, customer_id varchar(45) NOT NULL, time varchar(45), sale decimal(10,2) NOT NULL, price decimal(10,2) NOT NULL, week int(11)NOT NULL)");            
             ps.executeUpdate();
-            ps = conn.prepareStatement("Insert into fact select product_id,amount, store_id, customer_id, time, sum(sale) from transaction group by product_id,customer_id,store_id,time");
+            ps = conn.prepareStatement("Insert into fact select product_id, amount, store_id, customer_id, time, sum(sale), price*discount, week from transaction group by product_id,customer_id,store_id,time");
             ps.executeUpdate();
             
         } catch (SQLException se) {
