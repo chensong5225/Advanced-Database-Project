10296485dc72e6ace9dbe0b736c810d5860b702a
FleetFei <qifeish@gmail.com>
2017-04-16 19:37:11 -0400

    qifei

--- BookStoreProject/src/java/MongoServlet/MongoServletQ6.java
+++ BookStoreProject/src/java/MongoServlet/MongoServletQ6.java
@@ -11,13 +11,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author fei
  */
-public class MongoQ6 extends HttpServlet {
+public class MongoServletQ6 extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,21 +30,15 @@ public class MongoQ6 extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-         HttpSession session = request.getSession();
-        //获取前端数据
-        request.getParameter("");
-        
-        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet MongoQ1</title>");            
+            out.println("<title>Servlet MongoServletQ6</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet MongoQ1 at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet MongoServletQ6 at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
